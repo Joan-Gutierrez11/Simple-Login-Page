626c8434729587f6a2ae9da9aba61d9e3c10e640
JoSebas007 <joangutierrez2001@gmail.com>
2021-01-03 22:43:20 -0500

    "a small code was inserted in javascript"

--- Script/scripts.js
+++ Script/scripts.js
@@ -1,2 +1,18 @@
+function login(){
+    var username = document.getElementById("Username").value;
+    var password = document.getElementById("Password").value;
 
+    if(username != ""){
+        if(password != ""){
+            alert("Registered Successfully");
+            console.log(username + ":" + password);
+        }else{
+            alert("Enter the data correctly");
+            console.log("ERROR");
+        }
+    }else{
+        alert("Enter the data correctly");
+        console.log("ERROR");
+    }
+}
 
